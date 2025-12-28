721e242d6834282d353ed78ca90e54e4345623f3
EuJ <pzweuj@live.com>
2025-12-28 21:09:02 +0800

    [update] 权限管理

--- apps/web-germline/src/app/(main)/settings/components/index.ts
+++ apps/web-germline/src/app/(main)/settings/components/index.ts
@@ -1,3 +1,3 @@
 export { ProfileSettings } from './ProfileSettings';
-export { PermissionsManagement, ROLES, ROLE_PERMISSIONS } from './PermissionsManagement';
-export type { RoleValue } from './PermissionsManagement';
+export { PermissionsManagement, PERMISSIONS } from './PermissionsManagement';
+export type { PermissionId } from './PermissionsManagement';
